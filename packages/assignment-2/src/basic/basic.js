decf46c9f8e9b8f1fe7fa46d9cf6a1fb8baa3e20
4538asd@gmail.com <hs.kim@teamsparta.co>
2024-06-28 07:50:14 +0900

    feat : basic 테스트 통과

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -59,21 +59,20 @@ export function deepEquals(target1, target2) {
 }
 
 export function createNumber1(n) {
-  return new CustomNumber(n, "number");
+  return new Number1(n);
 }
 
 export function createNumber2(n) {
-  return new CustomNumber(n, "string");
+  return new Number2(n);
 }
 
 export function createNumber3(n) {
-  return new CustomNumber(n, "json");
+  return new Number3(n);
 }
 
-class CustomNumber {
-  constructor(value, type) {
+class CustomObject {
+  constructor(value) {
     this.value = value;
-    this.type = type;
   }
 
   valueOf() {
@@ -85,23 +84,175 @@ class CustomNumber {
   }
 
   toJSON() {
-    if (this.type === "json") {
-      return `${this.value}`;
-    }
-    return this.value;
+    return this.value.toString();
   }
 }
 
-export function createUnenumerableObject(target) {
-  return target;
+class Number1 extends CustomObject {
+  constructor(value) {
+    super(value);
+  }
 }
 
-export function forEach(target, callback) {}
+class Number2 extends CustomObject {
+  constructor(value) {
+    super(value);
+  }
 
-export function map(target, callback) {}
+  valueOf() {
+    return this.value.toString();
+  }
+}
 
-export function filter(target, callback) {}
+class Number3 extends CustomObject {
+  constructor(value) {
+    super(value);
+  }
 
-export function every(target, callback) {}
+  toJSON() {
+    return `this is createNumber3 => ${this.value}`;
+  }
+}
 
-export function some(target, callback) {}
+export class CustomNumber extends CustomObject {
+  static cache = {};
+
+  constructor(value) {
+    if (CustomNumber.cache.hasOwnProperty(value)) {
+      return CustomNumber.cache[value];
+    }
+    super(value);
+    CustomNumber.cache[value] = this;
+  }
+
+  toJSON() {
+    return this.value.toString();
+  }
+}
+
+const isArrayLike = (target) => {
+  if (target == null) {
+    return false;
+  }
+
+  if (Array.isArray(target)) {
+    return true;
+  }
+
+  const length = target.length;
+  if (typeof length === "number" && length >= 0 && Number.isFinite(length)) {
+    return true;
+  }
+
+  return false;
+};
+
+export function createUnenumerableObject(target) {
+  const newObj = {};
+  for (const key in target) {
+    Object.defineProperty(newObj, key, {
+      value: target[key],
+      enumerable: false,
+      writable: true,
+      configurable: true,
+    });
+  }
+  return newObj;
+}
+
+export function forEach(target, callback) {
+  if (isArrayLike(target)) {
+    const length = Array.from(target).length;
+    for (let i = 0; i < length; i++) {
+      callback(target[i], i);
+    }
+  } else if (typeof target === "object") {
+    const keys = Object.getOwnPropertyNames(target);
+    for (const key of keys) {
+      callback(target[key], key);
+    }
+  } else {
+    for (const key in target) {
+      if (target.hasOwnProperty(key)) {
+        callback(target[key], key);
+      }
+    }
+  }
+}
+
+export function map(target, callback) {
+  if (isArrayLike(target)) {
+    const result = [];
+    for (const item of Array.from(target)) {
+      result.push(callback(item));
+    }
+    return result;
+  } else if (typeof target === "object") {
+    const result = {};
+    for (const key of Object.getOwnPropertyNames(target)) {
+      result[key] = callback(target[key]);
+    }
+    return result;
+  }
+  return [];
+}
+
+export function filter(target, callback) {
+  if (isArrayLike(target)) {
+    const result = [];
+    for (const item of Array.from(target)) {
+      if (callback(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  } else if (typeof target === "object") {
+    const result = {};
+    for (const key of Object.getOwnPropertyNames(target)) {
+      if (callback(target[key])) {
+        result[key] = target[key];
+      }
+    }
+    return result;
+  }
+  return [];
+}
+
+export function every(target, callback) {
+  if (isArrayLike(target)) {
+    for (const item of Array.from(target)) {
+      if (!callback(item)) {
+        return false;
+      }
+    }
+    return true;
+  } else if (typeof target === "object") {
+    for (const key of Object.getOwnPropertyNames(target)) {
+      if (!callback(target[key])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return true;
+  }
+}
+
+export function some(target, callback) {
+  if (isArrayLike(target)) {
+    for (const item of Array.from(target)) {
+      if (callback(item)) {
+        return true;
+      }
+    }
+    return false;
+  } else if (typeof target === "object") {
+    for (const key of Object.getOwnPropertyNames(target)) {
+      if (callback(target[key])) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return true;
+}
