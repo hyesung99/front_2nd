f1d5228bc5d5b79c73cfbda8bc099c69951aee68
hyesung99 <hs.kim@teamsparta.co>
2024-07-13 12:13:53 +0900

    feat : create ShoppingCart component

---
+++ packages/assignment-4/src/basic/src/components/ShoppingCart.js
@@ -0,0 +1,94 @@
+import { Component } from "../core/component";
+
+export class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: {},
+    };
+    this.render();
+  }
+
+  createElement() {
+    const div = document.createElement("div");
+    div.className = "shopping-cart";
+    return div;
+  }
+
+  mount() {
+    this.addEventDelegate("click", ".quantity-change", (e) => {
+      const productId = e.target.dataset.productId;
+      const newState = { ...this.state };
+      newState.items[productId].quantity += parseInt(e.target.dataset.change);
+      if (newState.items[productId].quantity <= 0) {
+        delete newState.items[productId];
+      }
+      this.setState(newState);
+    });
+
+    this.addEventDelegate("click", ".remove-item", (e) => {
+      const productId = e.target.dataset.productId;
+
+      const newState = { ...this.state };
+      delete newState.items[productId];
+      this.setState(newState);
+    });
+  }
+
+  render() {
+    const cartItemsHtml = Object.entries(this.state.items)
+      .map(
+        ([id, item]) =>
+          `<div>
+        ${item.name} - ${item.price}원 x ${item.quantity}
+        <button class="quantity-change" data-change="-1" data-product-id="${id}">-</button>
+        <button class="quantity-change" data-change="1" data-product-id="${id}">+</button>
+        <button class="remove-item" data-product-id="${id}">삭제</button>
+      </div>`
+      )
+      .join("");
+
+    const total = Object.values(this.state.items).reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
+
+    this.element.innerHTML = `
+      <h1 class="text-2xl font-bold mb-4">장바구니</h1>
+      <div id="cart-items">${cartItemsHtml}</div>
+      <div id="cart-total" class="text-xl font-bold my-4">총액: ${total}원</div>
+    `;
+  }
+
+  addItem(product) {
+    const items = { ...this.state.items };
+    if (items[product.id]) {
+      items[product.id].quantity += 1;
+    } else {
+      items[product.id] = { ...product, quantity: 1 };
+    }
+
+    this.setState({ items });
+  }
+
+  updateQuantity(productId, change) {
+    this.setState((prevState) => {
+      const items = { ...prevState.items };
+      if (items[productId]) {
+        items[productId].quantity += change;
+        if (items[productId].quantity <= 0) {
+          delete items[productId];
+        }
+      }
+      return { items };
+    });
+  }
+
+  removeItem(productId) {
+    this.setState((prevState) => {
+      const items = { ...prevState.items };
+      delete items[productId];
+      return { items };
+    });
+  }
+}
