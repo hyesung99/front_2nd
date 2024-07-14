ca26b454644aad821a1358b04580b8b66d5d6a1f
hyesung99 <hs.kim@teamsparta.co>
2024-07-14 15:37:56 +0900

    feat : useCoupon 훅 완성

--- packages/assignment-5/src/refactoring/hooks/useCoupon.ts
+++ packages/assignment-5/src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,18 @@
-import { Coupon } from "../../types.ts";
 import { useState } from "react";
+import { Coupon } from "../model/coupons/coupons.type";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = ({ coupon }: { coupon: Coupon }) => {
+    setCoupons([...coupons, coupon]);
+  };
+
+  const removeCoupon = ({ coupon }: { coupon: Coupon }) => {
+    setCoupons(
+      coupons.filter((searchCoupon) => coupon.code !== searchCoupon.code)
+    );
+  };
+
+  return { coupons, addCoupon, removeCoupon };
 };
